f9aeafe0c77905ac76bc68144074b9e5f5e302cc
Mark Strendin <mark.strendin@gmail.com>
2020-06-10 14:42:18 -0600

    Add support for key vault configuration

--- APIClient/Program.cs
+++ APIClient/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 
 namespace APIClient
 {
     class Program
     {
+        static IConfiguration configuration = new ConfigurationBuilder()
+                   .AddEnvironmentVariables()
+                   .Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string azkvEndpoint = configuration["KEYVAULT_ENDPOINT"];
+            Console.WriteLine("> Connecting to Azure Key Vault: " + azkvEndpoint);            
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(
+                new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback));
+            configuration = new ConfigurationBuilder()
+                    .AddConfiguration(configuration)
+                    .AddAzureKeyVault(azkvEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager())
+                    .Build();
+
+            
         }
     }
 }
